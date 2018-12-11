1f7a4951a924eec313cff8a751ac8ded50452696
gurupatra <guru.patra@gmail.com>
2018-12-11 23:02:14 +0000

    added model

---
+++ src/main/java/com/prag/model/Destination.java
@@ -0,0 +1,60 @@
+package com.prag.model;
+
+import javax.validation.constraints.NotEmpty;
+
+public class Destination {
+    @NotEmpty
+    private String hostName;
+
+    @NotEmpty
+    private String port;
+
+    @NotEmpty
+    private String queueName;
+
+    @NotEmpty
+    private String queueManager;
+
+    @NotEmpty
+    private String messageText;
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getQueueName() {
+        return queueName;
+    }
+
+    public void setQueueName(String queueName) {
+        this.queueName = queueName;
+    }
+
+    public String getQueueManager() {
+        return queueManager;
+    }
+
+    public void setQueueManager(String queueManager) {
+        this.queueManager = queueManager;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+}
